bd54fb14d2a0f65860b917e35ec193db5fe09372
JohanCacerez <johancacerez10@gmail.com>
2025-07-23 20:58:33 -0600

    separar en servidor

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-import express from 'express';
+import server from './server';
 
-const app = express();
-             //variable de entorno para el puerto
+//variable de entorno para el puerto
 const port = process.env.PORT || 3000;
 
-//routing
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
