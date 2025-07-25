b1b1d7492cacb06ef37f0c8ea89f49262fe6e651
JohanCacerez <johancacerez10@gmail.com>
2025-07-24 22:13:06 -0600

    conectar con bd de mongo

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,15 @@ import express from 'express';
 //rutas
 import router from './router';
 
+//DB
+import {connectDB} from './config/db'
+
+//ENV
+import 'dotenv/config';
+
 const app = express();
+
+connectDB();
 
 //Leer datos del body
 app.use(express.json());
