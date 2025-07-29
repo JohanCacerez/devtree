70365da98b2fb27c240ee8044619f366d64e93dc
JohanCacerez <johancacerez10@gmail.com>
2025-07-28 19:26:32 -0600

    añadir handle a usaurios

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,16 +1,29 @@
 import mongoose, { Schema } from "mongoose";
 
+//definir una interfaz para el modelo de usuario
+//esto es para que TypeScript sepa qué propiedades tendrá el modelo de usuario
 interface IUser {
+    handle: string;
     name: string;
     email: string;
     password: string;
 }
 
+//definir el esquema del modelo de usuario
+//esto es para que Mongoose sepa cómo se verá el modelo de usuario en la base
 const userSchema = new Schema({
+    handle: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true, 
+    },
     name: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        lowercase: true
     },
     email: {
         type: String,
@@ -26,6 +39,8 @@ const userSchema = new Schema({
 
 })
 
+//definir el modelo de usuario
+//esto es para que Mongoose sepa cómo se verá el modelo de usuario en la base
 const UserModel = mongoose.model<IUser>("User", userSchema);
 
 export default UserModel;
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,17 +1,31 @@
 import { Request, Response } from "express";
+import slug from "slug";
 import UserModel from "../Models/User";
 import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
   const {email, password} = req.body;
 
-  //verificamos que el email no esten vacios
+  //verificamos que el email este disponible
   const userExists = await UserModel.findOne({ email });
 
-  //verificamos si el usuario ya existe
   if (userExists) {
     //creamos un error si el usuario ya existe
-    const error = new Error("User already exists");
+    const error = new Error("email already exists");
+    //mandamos el error al cliente y detenemos la ejecución
+    return res.status(409).json({error: error.message});
+  }
+
+  //asignamos un handle al usuario
+  //slug es una librería que convierte un string en un slug
+  const handle = slug(req.body.handle, '')
+
+  //verificar disponibilidad del handle
+  const handleExists = await UserModel.findOne({ handle });
+
+  if (handleExists) {
+    //creamos un error si el nombre ya existe
+    const error = new Error("Name already exists");
     //mandamos el error al cliente y detenemos la ejecución
     return res.status(409).json({error: error.message});
   }
@@ -20,6 +34,8 @@ export const createAccount = async (req: Request, res: Response) => {
   const User = new UserModel(req.body);
   //hasheamos la contraseña
   User.password = await hashPassword(password);
+  //asignamos el handle al usuario
+  User.handle = handle;
 
   //guardamos el usuario en la base de datos
   await User.save();
