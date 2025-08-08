0f637c10265b3b633526d41486aae4fc5948cf6c
JohanCacerez <johancacerez10@gmail.com>
2025-08-07 21:45:23 -0600

    identificar usaurio por jwt

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import slug from "slug";
+import jwt from "jsonwebtoken";
 import UserModel from "../Models/User";
 import { checkPassword, hashPassword } from "../utils/auth";
 import { generateJWT } from "../utils/jwt";
 
 export const createAccount = async (req: Request, res: Response) => {
-
   const { email, password } = req.body;
 
   //verificamos que el email este disponible
@@ -47,7 +47,6 @@ export const createAccount = async (req: Request, res: Response) => {
 };
 
 export const login = async (req: Request, res: Response) => {
-
   const { email, password } = req.body;
 
   //verificamos que el email este disponible
@@ -67,8 +66,44 @@ export const login = async (req: Request, res: Response) => {
     return res.status(401).json({ error: error.message });
   }
 
-  const token =generateJWT({id: user._id})
+  const token = generateJWT({ id: user._id });
 
   //si todo es correcto, devolvemos el usuario
-  res.send(token)
-}
+  res.send(token);
+};
+
+export const getUser = async (req: Request, res: Response) => {
+  const bearer = req.headers.authorization;
+
+  //verificamos que el token de autenticación exista
+  if (!bearer) {
+    const error = new Error("No se ha proporcionado un token de autenticación");
+    return res.status(401).json({ error: error.message });
+  }
+
+  //verificamos que el token de autenticación sea válido
+  const [, token] = bearer.split(" ");
+
+  //verificamos que el token no sea nulo o indefinido
+  if (!token) {
+    const error = new Error("Token de autenticación inválido");
+    return res.status(401).json({ error: error.message });
+  }
+
+  //verificamos el token y obtenemos el usuario
+  try {
+    // Verificamos el token
+    const result = jwt.verify(token, process.env.JWT_SECRET);
+    // Si el token es válido, buscamos al usuario por su ID
+    if (typeof result === "object" && result.id) {
+      const user = await UserModel.findById(result.id).select("-password -__v");
+      if (!user) {
+        const error = new Error("El usuario no existe");
+        return res.status(404).json({ error: error.message });
+      }
+      res.json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error al verificar el token" });
+  }
+};
